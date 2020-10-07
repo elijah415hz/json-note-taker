811c2f857ba22973bc554bade119bf81c086cbda
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-07 14:42:57 -0700

    Set static folder to connect css and js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+// Require packages
 const express = require("express");
 const fs = require("fs")
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static folder to "public"
+app.use(express.static("public")) 
+
+// Root path
 app.get("/", function(req, res) {
     fs.readFile(__dirname + "/public/index.html", "utf8", (err, data) => {
         if (err) throw err;
@@ -11,6 +20,7 @@ app.get("/", function(req, res) {
     })
 })
 
+// Serve notes.html
 app.get("/notes", function(req, res) {
     fs.readFile(__dirname + "/public/notes.html", "utf8", (err, data) => {
         if (err) throw err;
@@ -18,6 +28,7 @@ app.get("/notes", function(req, res) {
     })
 })
 
+// Set listening port
 app.listen(PORT, function() {
     console.log("Server listening on Port " + PORT)
 })
